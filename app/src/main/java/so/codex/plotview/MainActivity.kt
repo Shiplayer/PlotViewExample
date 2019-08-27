7c2142da48d215e347ef272646041f2255ff650b
Shiplayer <shiplayer@yandex.ru>
2019-08-27 13:18:32 +0300

    add data classes for point and labels, add lines for libels and rectangle for tests

--- app/src/main/java/so/codex/plotview/MainActivity.kt
+++ app/src/main/java/so/codex/plotview/MainActivity.kt
@@ -1,8 +1,12 @@
 package so.codex.plotview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.text.format.DateFormat
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        plotView.data = (1..10).map {
+            PlotView.Point(it, Random.nextInt(10, 1000))
+        }
+        val date = Date().time
+        val day = 1000 * 60 * 60 * 24
+        val calendar = Calendar.getInstance()
+        plotView.xLabel = (1..10).map {
+            calendar.timeInMillis = date - day
+            DateFormat.format("dd MMM", calendar.time).toString().also {
+                Log.i("MainActivity", "$it")
+            }
+        }
     }
 }
--- app/src/main/java/so/codex/plotview/PlotView.kt
+++ app/src/main/java/so/codex/plotview/PlotView.kt
@@ -1,15 +1,22 @@
 package so.codex.plotview
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.DashPathEffect
+import android.graphics.LinearGradient
 import android.graphics.Paint
-import android.graphics.drawable.Drawable
+import android.graphics.Path
+import android.graphics.PathMeasure
+import android.graphics.Rect
+import android.graphics.Shader
+import android.graphics.Typeface
+import android.text.TextPaint
+import android.text.TextUtils
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
 import kotlin.math.roundToInt
 import kotlin.properties.Delegates
 
@@ -18,18 +25,104 @@ class PlotView @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     //private val foregroundDrawable: Drawable = ContextCompat.getDrawable(context, R.drawable.background)!!
+    private var maxX = 0
     private var maxY = 0
-    private var minY = 0
-    private var data by Delegates.observable(listOf<Point>()) {_, new, old ->
-        maxY = new.maxBy { it.y }?.y ?: 0
-        minY = new.minBy { it.y }?.y ?: 0
-
+    private var paint = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.STROKE
+        strokeWidth = 2.toDp()
     }
 
-    private val startPoint = Point(0,0)
+    private val plotPaddingBottom = 16.toDp()
+    private val plotPaddingLeft = 0.toDp()
+
+    private var measure: PathMeasure? = null
+    public var data by Delegates.observable(listOf<Point>()) { _, old, new ->
+        post {
+            maxX = new.maxBy { it.x }?.x ?: 0
+            maxY = new.maxBy { it.y }?.y ?: 0
+            path.reset()
+            val width = width - plotPaddingLeft
+            val height = height - plotPaddingBottom
+            paint.shader = LinearGradient(
+                    0f,
+                    0f,
+                    0f,
+                    height,
+                    Color.RED,
+                    Color.BLACK,
+                    Shader.TileMode.REPEAT
+            )
+            val points = new.sortedBy { it.x }.takeLast(16)
+            val step = width / (points.size - 1)
+            Log.i("PlotView", "step point = $step")
+            points.forEachIndexed { i, it ->
+                val y = height - it.y.toFloat() / maxY * height
+                val x = plotPaddingLeft + (step * i)
+                Log.i("PlotViewPoint", "($x, $y) ($width, $height)")
+                if (i == 0) {
+                    path.moveTo(x, y)
+                }
+                path.lineTo(x, y)
+            }
+            measure = PathMeasure(path, false)
+            ObjectAnimator.ofFloat(this, "phase", 1f, 0f).apply {
+                duration = 2000
+                start()
+            }
+        }
+    }
+
+    private var mXLabel: List<Label> = listOf()
+    private val labelPaint = TextPaint().apply {
+        isAntiAlias = true
+        textSize = 10.toDp()
+        typeface = Typeface.create("roboto", Typeface.NORMAL)
+        color = Color.GRAY
+        isLinearText = true
+        textAlign = Paint.Align.LEFT
+    }
+
+    public var xLabel by Delegates.observable(listOf<String>()) { _, old, new ->
+        post {
+            val width = width - plotPaddingLeft
+            val step = width / (data.size - 1)
+            Log.i("PlotView", "step = $step")
+            val betweenPadding = 2.toDp()
+            mXLabel = List(data.size - 2) {
+                Label(
+                        TextUtils.ellipsize(
+                                new[it],
+                                labelPaint,
+                                step - betweenPadding * 2,
+                                TextUtils.TruncateAt.END
+                        ),
+                        (step * (it + 1)) - (step - betweenPadding * 2)/ 2 + betweenPadding * 2,
+                        height - 2.toDp()
+                ).also { l ->
+                    Log.i("PlotView", "Label$it text = ${l.text} x = ${l.x} y = ${l.y}")
+                }
+            }
+        }
+    }
+
+    private val path = Path()
 
     init {
+        Log.i("PlotView", "width = $width\theight = $height")
+    }
 
+    fun setPhase(phase: Float) {
+        Log.i("PlotView", "Phase is $phase")
+        paint.pathEffect = DashPathEffect(
+                floatArrayOf(
+                        measure?.length ?: 0f,
+                        measure?.length ?: 0f
+                ), phase * (measure?.length ?: 0f)
+        ).also {
+            Log.i("PlotView", "${measure?.length ?: 0f}, ${phase * (measure?.length ?: 0f)}")
+        }
+        invalidate()
     }
 
     override fun getMinimumWidth(): Int {
@@ -43,30 +136,30 @@ class PlotView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        canvas.drawRoundRect(
-                paddingLeft.toFloat(),
-                paddingTop.toFloat(),
-                (width - (paddingRight + paddingLeft)).toFloat(),
-                (height - (paddingBottom + paddingTop)).toFloat(),
-                8.toDp(),
-                8.toDp(),
-                Paint().apply {
-                    color = Color.DKGRAY
-                }
-        )
-
-        val bottomPlot = (height - (paddingBottom + paddingTop))
-        val leftPlot = (paddingLeft)
-
-        data.forEachIndexed {index, it ->
-            if(index == data.size)
-                return@forEachIndexed
-
+        canvas.drawPath(path, paint)
+        mXLabel.forEach {
+            canvas.drawText(it.text.toString(), it.x, it.y, labelPaint)
+            val rect = Rect()
+            labelPaint.getTextBounds(it.text.toString(),0, it.text.length, rect)
+            canvas.drawLine(it.x + rect.exactCenterX(), it.y, it.x + rect.exactCenterX(), 0f, Paint().apply {
+                isAntiAlias = true
+                color = Color.RED
+            })
+            rect.offsetTo(it.x.toInt(), it.y.toInt() - rect.height() + 1)
+            rect.left -= 2.toDp().toInt()
+            rect.right += 2.toDp().toInt()
+            canvas.drawRect(rect, Paint().apply {
+                isAntiAlias = true
+                color = Color.RED
+                style = Paint.Style.STROKE
+                strokeWidth = 1.toDp()
+            })
         }
 
     }
 
     fun Int.toDp(): Float = resources.displayMetrics.density * this
 
-    data class Point(val x: Int, val y:Int)
+    data class Point(val x: Int, val y: Int)
+    data class Label(val text: CharSequence, val x: Float, val y: Float)
 }
